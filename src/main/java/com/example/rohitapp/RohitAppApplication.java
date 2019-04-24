8d66b48cb55a8bc37095e0fb3c99dda60707c45a
USER <user@USER-no-MacBook-Air.local>
2019-04-24 22:01:59 +0900

    first commit

---
+++ src/main/java/com/example/rohitapp/RohitAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.rohitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RohitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RohitAppApplication.class, args);
+	}
+
+}
